3fcab5ac719f7fb68407f3452767efd3af730782
harshbhimra01 <harsh.bhimra@gmail.com>
2026-06-10 10:50:02 +0530

    updated24

--- practice-day24/azurerm_subnet/main.tf
+++ practice-day24/azurerm_subnet/main.tf
@@ -1,13 +1,7 @@
 resource "azurerm_subnet" "subnet" {
   for_each = var.subnet-map
-
   name = each.value.name
-
   resource_group_name = azurerm_virtual_network.vnet[each.value.vnet_reference].resource_group_name
-
   address_prefixes = each.value.address_prefixes
-
   virtual_network_name = azurerm_virtual_network.vnet[each.value.vnet_reference].name
-
-
 }
